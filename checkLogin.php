7d95a59419e3edeb787479dde9ec9ec216a44866
Nan Zheng <chunnan.zheng@uq.net.au>
2016-09-15 14:20:14 +1000

    change log in modal in index.html

---
+++ checkLogin.php
@@ -0,0 +1,51 @@
+<?php
+$dsn         = 'mysql:dbname=Mycauser;host=Localhost';
+$db_user     = "root";
+$db_password = "";
+
+try {
+    $conn = new PDO($dsn, $db_user, $db_password);
+}
+catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if (isset($_POST['submit'])) {
+    if ($username == "" && $password == "") {
+        
+        $_SESSION['error'] = "Nothing Entered";
+        //header('location: index.html');
+    } else {
+        $stmt  = "SELECT * FROM account WHERE username=? AND password=?";
+        $query = $conn->prepare($stmt);
+        $query->bindParam(1, $username);
+        $query->bindParam(2, $password);
+        
+        $query->execute();
+        
+        if ($query->rowCount() == 1) {
+            $_SESSION["auth"]     = true;
+            $_SESSION['error']    = "";
+            $_SESSION["username"] = $username;
+            
+            echo "success";
+            // header('location: index.html');
+            // echo $_SESSION["auth"];
+            exit;
+        } else {
+            echo "error";
+            $_SESSION['error'] = "error Username or Password!!";
+            // header('location: index.html');
+        }
+    }
+} else {
+    // error message
+    $_SESSION['error'] = "something wrong";
+    // header('location: index.html');
+}
+?>
