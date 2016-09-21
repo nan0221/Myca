071b5b8419f4e47f61c0b80ede5a6a3f769dde13
LAPTOP-IAPB1PKL\Holly <holly_dou@outlook.com>
2016-09-21 12:23:02 +1000

    Fix small bugs & add map position

--- js/script.js
+++ js/script.js
@@ -371,7 +371,30 @@ $(document).ready(function () {
     }
 });
 
+$(document).ready(function geoFindMe() {
+	var output = document.getElementById("out");
+	if (!navigator.geolocation){
+		output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    output.innerHTML = '<p>Latitude is ' + latitude + ' <br>Longitude is ' + longitude + '</p>';
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=400x400&sensor=false";
 
+    output.appendChild(img);
+  };
+
+function error() {
+    output.innerHTML = "Please use Internet Explorer";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+})
 
 
 var loadedImages = [];
